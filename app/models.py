fb9bef77c2f586c288f6a9495b0258f40a26a334
A1astorrr <meshcheryakov.v@list.ru>
2024-10-03 11:56:48 +0300

    create models

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+from .datebase import Base
+
+class NoteDB(Base):
+    # create table name
+    __tablename__ = "notes"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    text = Column(String)
+    # date of creation
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    # date of update
+    update_at = Column(DateTime(timezone=True), onupdate=func.now())
